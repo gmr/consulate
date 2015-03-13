23ad46697d408f51646e5a022d5eced86675bb41
Gavin M. Roy <gavinr@aweber.com>
2015-03-13 15:47:23 -0400

    Bump version and add NullHandler
    Prevent logging warnings when consulate does not have a handler

--- consulate/__init__.py
+++ consulate/__init__.py
@@ -2,7 +2,7 @@
 Consulate: A client library for Consul
 
 """
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from consulate.api import Session
 from consulate.api import TornadoSession
@@ -10,3 +10,17 @@ from consulate.api import TornadoSession
 # Backwards compatibility with 0.2.0
 Consulate = Session
 TornadoConsulate = TornadoSession
+
+import logging
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        """Python 2.6 does not have a NullHandler"""
+        def emit(self, record):
+            """Emit a record
+            :param record record: The record to emit
+            """
+            pass
+
+logging.getLogger('consulate').addHandler(NullHandler())
