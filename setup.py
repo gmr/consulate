ddb7cd97c4d1c757344435556cb4a15c0cb5e27e
Gavin M. Roy <gavinr@aweber.com>
2015-03-14 01:03:58 -0400

    Bump the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (2, 7, 0):
     install_requires.append('argparse')
 
 setup(name='consulate',
-      version='0.3.0',
+      version='0.4.0',
       description="A Client library for the Consul",
       maintainer="Gavin M. Roy",
       maintainer_email="gavinr@aweber.com",
