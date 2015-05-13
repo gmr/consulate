623e8c81402cd04a4bb662a15603f88bd4edefc7
Gavin M. Roy <gavinmroy@gmail.com>
2015-05-12 23:06:37 -0400

    Additional code duplication cleanup for Session

--- consulate/api/session.py
+++ consulate/api/session.py
@@ -9,7 +9,7 @@ class Session(base.Endpoint):
     """Create, destroy, and query Consul sessions."""
 
     def create(self, name=None, behavior='release', node=None, delay=None,
-               ttl=None, checks=None, datacenter=None):
+               ttl=None, checks=None, dc=None):
         """Initialize a new session.
 
         None of the fields are mandatory, and in fact no body needs to be PUT
@@ -39,9 +39,9 @@ class Session(base.Endpoint):
         highly recommended that, if you override this list, you include the
         default "serfHealth".
 
-        By default, the agent's local datacenter is used and you can specify
-        another datacenter, However, it is not recommended to use
-        cross-datacenter sessions.
+        By default, the agent's local dc is used and you can specify
+        another dc, However, it is not recommended to use
+        cross-dc sessions.
 
         :param str name: A human readable session name
         :param str behavior: One of ``release`` or ``delete``
@@ -49,12 +49,11 @@ class Session(base.Endpoint):
         :param str delay: A lock delay for the session
         :param str ttl: The time to live for the session
         :param lists checks: A list of associated health checks
-        :param str datacenter: The datacenter to create the session on
+        :param str dc: The data center to create the session on
         :return str: session ID
 
         """
         payload = {'name': name} if name else {}
-        query_param = {'dc': datacenter} if datacenter else {}
         if node:
             payload['Node'] = node
         if behavior:
@@ -65,59 +64,70 @@ class Session(base.Endpoint):
             payload['TTL'] = ttl
         if checks:
             payload['Checks'] = checks
-        return self._put_response_body(['create'], query_param,
+        return self._put_response_body(['create'], self._query(dc),
                                        payload).get('ID')
 
-    def destroy(self, session_id, datacenter=None):
+    def destroy(self, session_id, dc=None):
         """Destroy an existing session
 
         :param str session_id: The session to destroy
+        :param str dc: The optional data center to destroy the session from
         :return: bool
 
         """
-        query_param = {'dc': datacenter} if datacenter else {}
-        return self._put_no_response_body(['destroy', session_id], query_param)
+        return self._put_no_response_body(['destroy', session_id],
+                                          self._query(dc))
 
-    def info(self, session_id, datacenter=None):
-        """Returns the requested session information within a given datacenter.
-        By default, the datacenter of the agent is queried.
+    def info(self, session_id, dc=None):
+        """Returns the requested session information within a given dc.
+        By default, the dc of the agent is queried.
 
         :param str session_id: The session to get info about
+        :param str dc: The optional data center to get session info from
         :return: dict
 
         """
-        query_param = {'dc': datacenter} if datacenter else {}
-        return self._get_response_body(['info', session_id], query_param)
+        return self._get_response_body(['info', session_id], self._query(dc))
 
-    def list(self, datacenter=None):
-        """Returns the active sessions for a given datacenter.
+    def list(self, dc=None):
+        """Returns the active sessions for a given dc.
 
+        :param str dc: The optional data center to get a session list from
         :return: list
 
         """
-        query_param = {'dc': datacenter} if datacenter else {}
-        return self._get_response_body(['list'], query_param)
+        return self._get_response_body(['list'], self._query(dc))
 
-    def node(self, node, datacenter=None):
-        """Returns the active sessions for a given node and datacenter.
-        By default, the datacenter of the agent is queried.
+    def node(self, node, dc=None):
+        """Returns the active sessions for a given node and dc.
+        By default, the dc of the agent is queried.
 
         :param str node: The node to get active sessions for
+        :param str dc: The optional data center to get node session data from
         :return: list
 
         """
-        query_param = {'dc': datacenter} if datacenter else {}
-        return self._get_response_body(['node', node], query_param)
+        return self._get_response_body(['node', node], self._query(dc))
 
-    def renew(self, session_id, datacenter=None):
+    def renew(self, session_id, dc=None):
         """Renew the given session. This is used with sessions that have a TTL,
-        and it extends the expiration by the TTL. By default, the datacenter
+        and it extends the expiration by the TTL. By default, the dc
         of the agent is queried.
 
         :param str session_id: The session to renew
+        :param str dc: The optional data center to for the session renewal
         :return: dict
 
         """
-        query_param = {'dc': datacenter} if datacenter else {}
-        return self._put_response_body(['renew', session_id], query_param)
+        return self._put_response_body(['renew', session_id], self._query(dc))
 
+    @staticmethod
+    def _query(dc):
+        """Return the query args for a request specifying the data center
+        if it is specified.
+
+        :param str dc: The data center name
+        :rtype: dict
+
+        """
+        return {'dc': dc} if dc else {}
