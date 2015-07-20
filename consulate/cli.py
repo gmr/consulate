2e6ed045c1364ec9833a22a919562bd39a8abc50
Matt Walker <matt@suckerfish.net>
2015-07-20 16:19:44 -0700

    Added --recurse and --trim to cli kv_get

--- consulate/cli.py
+++ consulate/cli.py
@@ -53,7 +53,11 @@ KV_PARSERS = [
         [['key'], {'help': 'The key to get'}],
         [['-r', '--recurse'],
          {'help': 'Get all keys prefixed with the specified key',
-          'action': 'store_true'}]]),
+          'action': 'store_true'}],
+        [['-t', '--trim'],
+         {'help': 'Number of levels of prefix to trim from returned key',
+          'type': int,
+          'default': 0}]]),
     ('set', 'Set a key in the database', [
         [['key'], {'help': 'The key to set'}],
         [['value'], {'help': 'The value of the key'}]]),
@@ -213,9 +217,16 @@ def kv_get(consul, args):
     try:
         if args.recurse:
             for key in sorted(consul.kv.find(args.key)):
-                sys.stdout.write("%s\t%s\n" % (key, consul.kv.get(key)))
+                displaykey = key
+                if args.trim:
+                    keyparts = displaykey.split('/')
+                    if (args.trim >= len(keyparts)):
+                        displaykey = keyparts[-1]
+                    else:
+                        displaykey = '/'.join(keyparts[args.trim:])
+                sys.stdout.write('%s\t%s\n' % (displaykey, consul.kv.get(key)))
         else:
-            sys.stdout.write("%s\n" % consul.kv.get(args.key))
+            sys.stdout.write('%s\n' % consul.kv.get(args.key))
     except exceptions.ConnectionError:
         connection_error()
 
