45e9957ce7e91ec6b71488e576bac89a2a40c29d
Gavin M. Roy <gavinr@aweber.com>
2018-02-26 15:24:04 -0500

    Cleanup setup.py, drop python 2.6

--- setup.py
+++ setup.py
@@ -1,39 +1,35 @@
-from setuptools import setup
-import sys
+import setuptools
 
-install_requires = ['requests>=2.0.0,<3.0.0']
-
-extras_require = {'unixsocket': ['requests-unixsocket>=0.1.4,<=1.0.0'] }
-
-if sys.version_info < (2, 7, 0):
-    install_requires.append('argparse')
-
-setup(name='consulate',
-      version='0.7.0-rc0',
-      description="A Client library for the Consul",
-      maintainer="Gavin M. Roy",
-      maintainer_email="gavinr@aweber.com",
-      url="https://consulate.readthedocs.org",
-      install_requires=install_requires,
-      extras_require=extras_require,
-      license='BSD',
-      package_data={'': ['LICENSE', 'README.rst']},
-      packages=['consulate', 'consulate.api'],
-      entry_points=dict(console_scripts=['consulate=consulate.cli:main']),
-      classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: Implementation :: CPython',
-                   'Programming Language :: Python :: Implementation :: PyPy',
-                   'Topic :: System :: Systems Administration',
-                   'Topic :: System :: Clustering',
-                   'Topic :: Internet :: WWW/HTTP',
-                   'Topic :: Software Development :: Libraries'],
-      zip_safe=True)
+setuptools.setup(
+    name='consulate',
+    version='1.0.0',
+    description='A Client library and command line application for the Consul',
+    maintainer='Gavin M. Roy',
+    maintainer_email='gavinr@aweber.com',
+    url='https://consulate.readthedocs.org',
+    install_requires=['requests>=2.0.0,<3.0.0'],
+    extras_require={'unixsocket': ['requests-unixsocket>=0.1.4,<=1.0.0']},
+    license='BSD',
+    package_data={'': ['LICENSE', 'README.rst']},
+    packages=['consulate', 'consulate.api'],
+    entry_points=dict(console_scripts=['consulate=consulate.cli:main']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Clustering',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=True)
