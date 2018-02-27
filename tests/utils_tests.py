7be8eded18810efb4fdff66829cd9f064f373f00
Gavin M. Roy <gavinr@aweber.com>
2018-02-27 17:07:50 -0500

    Improve coverage

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import unittest
 
-from consulate import utils
+from consulate import exceptions, utils
 
 
 class QuoteTestCase(unittest.TestCase):
@@ -17,6 +17,43 @@ class MaybeEncodeTestCase(unittest.TestCase):
     @unittest.skipUnless(utils.PYTHON3, 'Python3 Only')
     def byte_test(self):
         self.assertEqual(utils.maybe_encode(b'bar'), b'bar')
+
+
+class Response(object):
+    def __init__(self, status_code=200, body=b'content'):
+        self.status_code = status_code
+        self.body = body
+
+
+class ResponseOkTestCase(unittest.TestCase):
+
+    def test_200(self):
+        self.assertTrue(utils.response_ok(Response(200, b'ok')))
+
+    def test_400(self):
+        with self.assertRaises(exceptions.ClientError):
+            utils.response_ok(Response(400, b'Bad request'))
+
+    def test_401(self):
+        with self.assertRaises(exceptions.ACLDisabled):
+            utils.response_ok(Response(401, b'What ACL?'))
+
+    def test_403(self):
+        with self.assertRaises(exceptions.Forbidden):
+            utils.response_ok(Response(403, b'No'))
+
+    def test_404_not_raising(self):
+        self.assertFalse(utils.response_ok(Response(404, b'not found')))
+
+    def test_404_raising(self):
+        with self.assertRaises(exceptions.NotFound):
+            utils.response_ok(Response(404, b'Not Found'), True)
+
+    def test_500(self):
+        with self.assertRaises(exceptions.ServerError):
+            utils.response_ok(Response(500, b'Opps'))
+
+
 
 
 class ValidateGoDurationTestCase(unittest.TestCase):
@@ -43,3 +80,5 @@ class ValidateURLTestCase(unittest.TestCase):
         for value in {'localhost', 'a'}:
             print('Testing {}'.format(value))
             self.assertFalse(utils.validate_url(value))
+
+
